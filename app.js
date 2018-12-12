0d746f43517023b8d0851b954c67ed8dd0004529
paulwillis <pwillis@truvista.biz>
2018-12-12 15:25:52 -0500

    add validator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
+var path = require('path'); 
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -34,6 +35,13 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 //end - middleware for static folder
 
+ 
+//begin - validator
+app.use(expressValidator()); 
+//end - validator
+
+
+
 
 //create json object
 var people = [
@@ -47,15 +55,55 @@ var people = [
     }
 ]
 
+var users = [
+    {
+        id: 1,
+        first_name: 'John',
+        last_name: 'Smith',
+        email: 'aosrij@oi.com'
+    },
+    {
+        id: 2,
+        first_name: 'Jill',
+        last_name: 'Doe',
+        email: 'fert@gyue.com'
+    }
+]
+
 
 //Route handler
 app.get('/', function(req, res){
     //res.send('hello');
     //res.json(people);
     res.render('index',{
-        title:'Customers'
+        title:'Customers',
+        users:users
     });//for ejs
 })
+
+
+//Route handler
+app.post('/users/add', function(req, res){
+    req.checkBody('first_name', 'First name is required.').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        console.log('errors');
+    } else {
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+        }
+        console.log('yes!');
+    }
+
+    console.log(newUser);
+})
+
+ 
+
+
 
 app.listen(3000, function(){
     console.log('server started on port 3000...')
